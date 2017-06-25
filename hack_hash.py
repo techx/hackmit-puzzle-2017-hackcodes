db5ed11a4cb7feb0b1cd43ad81cacfdc80f8032e
Patrick Insinger <patins@users.noreply.github.com>
2017-06-25 11:45:49 -0400

    Drop python2 compat, improve hack-hash (#9)

--- hack_hash.py
+++ hack_hash.py
@@ -1,19 +1,33 @@
 import hashlib
+import math
 from builtins import bytes
-import struct
 
 
 def hack_hash(message, charset, length):
     """
-    Return a 64-character string that is the hash of message.
+    Hash used across Hack puzzles for secure but stateless
+    info transfer.
 
-    given a unicode message and charset list
+    Args:
+        message (str): the message to hash
+        charset (:obj:`list` of :obj:`str`): a list of the possible elements
+        length (int): the desired number of elements to concatenate in hash
+
+    Returns:
+        str: Concatenated length-long list consisting of charsets.
     """
     digests = []
     i = 0
-    while len(digests) * 64 < length:
+    bits_for_charset = int(math.ceil(math.log2(len(charset))))
+    bytes_for_charset = int(math.ceil(bits_for_charset/8))
+    while len(digests) * 64 < length * bytes_for_charset:
         digests.append(hashlib.sha512(
             bytes(str(i) + message, 'utf-8')).digest())
         i += 1
     digest = bytes(b'').join(digests)
-    return ''.join([charset[int(b) % len(charset)] for b in digest])[:length]
+    out = []
+    for i in range(length):
+        b = digest[i*bytes_for_charset:(i+1)*bytes_for_charset]
+        n = int.from_bytes(b, byteorder='little', signed=False)
+        out.append(charset[n % len(charset)])
+    return ''.join(out)
